2e90dbda5d6d6a13f61ccf22a9e882fad070dd12
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2024-09-08 21:26:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./Components/Homepage/Homepage";
+
+function App() {
+
+  return (
+    <>
+        <HomePage></HomePage>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hamburger/Hamburger.jsx
@@ -0,0 +1,41 @@
+// GlassmorphicHamburgerMenu.jsx
+import React, { useState } from 'react';
+import './Hamburger.css';
+
+const GlassmorphicHamburgerMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+    
+
+  return (
+    <div className="hamburger-menu">
+      <button className={`hamburger-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+        <div className="hamburger-menu">
+      <button className={`hamburger-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav className={`menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">Games</a></li>
+          <li><a href="/services">Faq</a></li>
+          <li><a href="/contact">Chat</a></li>
+        </ul>
+      </nav>
+    </div>
+    </div>
+
+  );
+};
+
+export default GlassmorphicHamburgerMenu;
+
